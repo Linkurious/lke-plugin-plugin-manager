a9eb59c741b3ecd286f230c82c09bad81cee505a
fantandrea <fantandrea@users.noreply.github.com>
2023-01-24 19:02:54 +0100

    LKE-7336 frontend lint

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,10 +1,6 @@
-import {spawn} from 'child_process';
-import {errorMonitor} from 'events';
-import {request} from 'http';
-import {traceDeprecation} from 'process';
+import {Manifest} from '../backend/PluginParser';
 
-import {response} from 'express';
-
+// FIXME: replace with import {InstalledPlugin} from '@linkurious/rest-client';
 type LKEPLugin = {
   filename: string;
   name: string;
@@ -37,25 +33,7 @@ function toggleAddMenu() {
   menu?.classList.contains('show') ? menu?.classList.remove('show') : menu?.classList.add('show');
 }
 
-function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function getTable(id: any): Promise<HTMLElement> {
-  while (document.getElementById(id) == null) {
-    await delay(100);
-  }
-  return document.getElementById(id) as HTMLElement;
-}
-
-async function getElement(id: any): Promise<HTMLElement> {
-  while (document.querySelector(id) == null) {
-    await delay(100);
-  }
-  return document.querySelector(id) as HTMLElement;
-}
-
-//Manage Plugins Tab
+// Manage Plugins Tab
 async function managePlugins() {
   try {
     const table = document.querySelector('#manageTable tbody')! as HTMLTableElement;
@@ -63,7 +41,7 @@ async function managePlugins() {
 
     const requestEnabled = await fetch(`api/plugins?filter=enabled`);
     if (requestEnabled.status === 200) {
-      let res = await requestEnabled.json();
+      let res = (await requestEnabled.json()) as Record<string, Manifest>;
       let plugins = Object.keys(res);
 
       for (let i = 0; i < plugins.length; i++) {
@@ -105,14 +83,14 @@ async function managePlugins() {
         const disable = document.createElement('button');
         disable.setAttribute('class', 'button hasNext');
         disable.setAttribute('data-plugin', plugins[i]);
-        disable.addEventListener('click', () => changeState(disable.dataset.plugin as string));
+        disable.addEventListener('click', () => void changeState(disable.dataset.plugin as string));
         disable.innerText = 'Disable';
         manage.append(disable);
         //--remove button
         const remove = document.createElement('button');
         remove.setAttribute('class', 'button');
         remove.setAttribute('data-plugin', plugins[i]);
-        remove.addEventListener('click', () => removePlugin(remove.dataset.plugin as string));
+        remove.addEventListener('click', () => void removePlugin(remove.dataset.plugin as string));
         remove.innerText = 'Remove';
         manage.append(remove);
         tr.append(manage);
@@ -133,7 +111,7 @@ async function managePlugins() {
 
     const requestDisabled = await fetch(`api/plugins?filter=disabled`);
     if (requestDisabled.status === 200) {
-      let res = await requestDisabled.json();
+      let res = (await requestDisabled.json()) as Record<string, Manifest>;
       let plugins = Object.keys(res);
 
       for (let i = 0; i < plugins.length; i++) {
@@ -176,14 +154,14 @@ async function managePlugins() {
         const enable = document.createElement('button');
         enable.classList.add('button', 'hasNext');
         enable.setAttribute('data-plugin', plugins[i]);
-        enable.addEventListener('click', () => changeState(enable.dataset.plugin as string));
+        enable.addEventListener('click', () => void changeState(enable.dataset.plugin as string));
         enable.innerText = 'Enable';
         manage.append(enable);
         //--remove button
         const remove = document.createElement('button');
         remove.classList.add('button');
         remove.setAttribute('data-plugin', plugins[i]);
-        remove.addEventListener('click', () => removePlugin(plugins[i]));
+        remove.addEventListener('click', () => void removePlugin(plugins[i]));
         remove.innerText = 'Remove';
         manage.append(remove);
         tr.append(manage);
@@ -202,14 +180,14 @@ async function managePlugins() {
 }
 
 //Status Tab
-async function status() {
+async function pluginStatus() {
   try {
     const request = await fetch(`../../api/admin/plugins`);
     if (request.status === 200) {
       const table = document.querySelector('#statusTable tbody')! as HTMLTableElement;
       const tbody = document.createElement('tbody');
 
-      const res = await request.json();
+      const res = (await request.json()) as Record<string, LKEPLugin>;
       const plugins = Object.keys(res);
 
       for (let i = 0; i < plugins.length; i++) {
@@ -256,7 +234,7 @@ async function status() {
         open.classList.add('button');
         open.setAttribute('data-plugin', act.basePath);
         open.innerText = 'Open';
-        if (act.state != 'error-manifest') {
+        if (act.state !== 'error-manifest') {
           open.addEventListener('click', () => window.open(`api/logs/${act.basePath}`, '_blank'));
         } else {
           open.disabled = true;
@@ -282,18 +260,18 @@ async function backup() {
   try {
     const request = await fetch(`api/plugins?filter=backup`);
     if (request.status === 204) {
+      /* empty */
     } else {
+      /* empty */
     }
     const table = document.querySelector('#backupTable tbody')! as HTMLTableElement;
 
     const tbody = document.createElement('tbody');
 
-    const res = await request.json();
+    const res = (await request.json()) as Record<string, Manifest>;
     const plugins = Object.keys(res);
 
     for (let i = 0; i < plugins.length; i++) {
-      const act: LKEPLugin = res[plugins[i]] as LKEPLugin;
-
       const tr = document.createElement('tr');
       tr.setAttribute('id', `backup-${plugins[i]}`);
 
@@ -309,7 +287,7 @@ async function backup() {
       restore.setAttribute('class', 'button');
       restore.setAttribute('data-plugin', plugins[i]);
       restore.innerText = 'Restore';
-      restore.addEventListener('click', () => restorePlugin(restore.dataset.plugin as string));
+      restore.addEventListener('click', () => void restorePlugin(restore.dataset.plugin!));
       manage.append(restore);
       tr.append(manage);
 
@@ -343,7 +321,7 @@ async function restorePlugin(plugin: string) {
   startWaiting();
   try {
     const request = await fetch(`api/plugin/${plugin}/restore`, {method: 'PATCH'});
-    if (request.status != 204) {
+    if (request.status !== 204) {
       const error: Error = (await request.json()) as Error;
       showErrorPopup(error.message);
     }
@@ -390,34 +368,31 @@ async function changeState(plugin: string) {
 }
 
 async function readFile() {
-  let input = document.getElementById('importFile') as any;
+  let input = document.getElementById('importFile') as HTMLInputElement;
   input = input.files[0];
 
   if (!input || !input.name.endsWith('.lke')) {
-    const error = await getElement('#fileError');
+    const error = document.getElementById('fileError') as HTMLDivElement;
     error.innerHTML = 'Select a valid file! ';
   } else {
     startWaiting();
-    await delay(1000);
   }
   stopWaiting();
 }
 
 window.addEventListener('load', () => {
   fetch(`api/authorize`)
-    .then((response) => {
+    .then(async (response) => {
       if (response.status === 204) {
         document.getElementById('addButton')!.onclick = toggleAddMenu;
         document.getElementById('closePopup')!.onclick = closeErrorPopup;
         document.getElementById('installButton')!.onclick = readFile;
 
-        document.getElementById('tab-1')!.onclick = status;
+        document.getElementById('tab-1')!.onclick = pluginStatus;
         document.getElementById('tab-2')!.onclick = managePlugins;
         document.getElementById('tab-3')!.onclick = backup;
 
-        status();
-        // managePlugins();
-        // backup();
+        await pluginStatus();
       } else {
         const close = document.getElementById('closePopup') as HTMLElement;
         close.parentElement?.removeChild(close);
@@ -433,6 +408,6 @@ window.addEventListener('load', () => {
 
       document.getElementById('errorPopup')?.classList.add('hider');
 
-      showErrorPopup(error.message);
+      showErrorPopup(error instanceof Error ? error.message : JSON.stringify(error));
     });
 });
